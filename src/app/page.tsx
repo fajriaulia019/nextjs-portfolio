fcc68faa9f9dbf94287c44f10627caaea30e836d
fajriaulia019 <fajriaulia019@gmail.com>
2026-06-20 21:25:24 +0700

    Initial setup nextjs portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1 className="font-bold text-3xl text-center">Fajri Kamu Keren, Tapi Ini Masih Permulaan</h1>
+        <h1 className="font-bold text-3xl text-center">Fajri Kamu Keren, Tapi Ini Masih Permulaan Dari Semua Stress Yang Akan Terjadi Nanti!!</h1>
       </main>
     </div>
   );
